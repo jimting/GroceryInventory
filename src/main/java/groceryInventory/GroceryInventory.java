929ffe53c7cf1a9e602aad487901ed64c0a6e938
jimting <handsome841206@gmail.com>
2019-01-09 20:54:00 +0800

    try

--- src/main/java/groceryInventory/GroceryInventory.java
+++ src/main/java/groceryInventory/GroceryInventory.java
@@ -1,10 +1,15 @@
 package groceryInventory;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.List;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
 
 public class GroceryInventory {
 	public static String getGrocery(String userID) 
@@ -12,13 +17,32 @@ public class GroceryInventory {
 		//給user拿到所有的資訊
 		//先把資料結構預設出來
 		//{"status":"404代表未登入/200代表已登入", "name":"使用者名字", "notification":[{"content":"通知內容", "status":"unread / read 有沒有看過此通知"},{...},...], "items":[{"ID":"商品編號", "name":"商品名稱", "content":"商品介紹", "quantity":"剩餘數量", "pic":"商品照片url"},{...},...]}
-		String item = getItem();
+		//String item = getItem();
 		String not = getNotification();
-		String result = "{\"status\":\"200\", \"name\":\"使用者名字\", \"notification\":"+not+", \"items\":"+item+"}";
-		return result;
+		//String result = "{\"status\":\"200\", \"name\":\"使用者名字\", \"notification\":"+not+", \"items\":"+item+"}";
+		
+		try {
+			MongoClient mongoClient = new MongoClient( "140.121.196.23" , 4118 );
+			DB db = mongoClient.getDB("Grocery");
+			boolean auth = db.authenticate("cinema", "cinema".toCharArray());
+			List<String> dbs = mongoClient.getDatabaseNames();
+			for(String item : dbs){
+				System.out.println(item);
+			}
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return "hello";
 	}
 	public static String getItem() 
 	{
+		
+		
 		//拿取所有商品資訊
 		String result = "[{\"ID\":\"1\", \"name\":\"商品名稱1\", \"content\":\"商品介紹1\", \"quantity\":\"30\", \"pic\":\"商品照片url\"},{\"ID\":\"1\", \"name\":\"商品名稱2\", \"content\":\"商品介紹2\", \"quantity\":\"40\", \"pic\":\"商品照片url\"},{\"ID\":\"1\", \"name\":\"商品名稱2\", \"content\":\"商品介紹2\", \"quantity\":\"50\", \"pic\":\"商品照片url\"}]";
 		return result;
@@ -45,5 +69,8 @@ public class GroceryInventory {
 		return "[]";
 	}
 	
-
+	public static void main()
+	{
+		String result = getGrocery("1");
+	}
 }

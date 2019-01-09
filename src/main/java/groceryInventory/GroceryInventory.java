b03642a626b32248aa4620d41053fc920f2a6c99
jimting <handsome841206@gmail.com>
2019-01-09 21:15:46 +0800

    update

--- src/main/java/groceryInventory/GroceryInventory.java
+++ src/main/java/groceryInventory/GroceryInventory.java
@@ -2,14 +2,19 @@ package groceryInventory;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 public class GroceryInventory {
 	public static String getGrocery(String userID) 
@@ -20,22 +25,45 @@ public class GroceryInventory {
 		//String item = getItem();
 		String not = getNotification();
 		//String result = "{\"status\":\"200\", \"name\":\"使用者名字\", \"notification\":"+not+", \"items\":"+item+"}";
-		
-		try {
-			MongoClient mongoClient = new MongoClient( "140.121.196.23" , 4118 );
-			DB db = mongoClient.getDB("Grocery");
-			boolean auth = db.authenticate("cinema", "cinema".toCharArray());
-			List<String> dbs = mongoClient.getDatabaseNames();
-			for(String item : dbs){
-				System.out.println(item);
-			}
+		try {  
+            
+			System.out.println("MongoDBConnect to database begin");
+            //連線到MongoDB服務 如果是遠端連線可以替換“localhost”為伺服器所在IP地址
+			
+            //MongoCredential.createScramSha1Credential()三個引數分別為 使用者名稱 資料庫名稱 密碼
+            MongoCredential credential = MongoCredential.createScramSha1Credential("cinema", "Movies", "cinema".toCharArray());
+            List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+            credentials.add(credential);
+
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .credential(credential)
+                    .applyToSslSettings(builder -> builder.enabled(true))
+                    .applyToClusterSettings(builder -> 
+                        builder.hosts(Arrays.asList(new ServerAddress("140.121.196.23", 4118))))
+                    .build();
+
+            
+            //通過連線認證獲取MongoDB連線
+            MongoClient mongoClient = MongoClients.create(settings);
+            
+            //連線到資料庫(schema)
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("Movies");
+            System.out.println("MongoDBConnect to database successfully");
+
+            //建立集合
+//            mongoDatabase.createCollection("test");
+//            System.out.println("集合建立成功");
+//選擇集合
+            MongoCollection collection = mongoDatabase.getCollection("Movie");
 			
 			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+			
+            System.out.println("Connect to database successfully");
+            return "Connect to database successfully:\n" + collection.find(Filters.text("A Land Imaged"));
+        } catch (Exception e) {  
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            return "Connect to database falily: "+e;
+        }
 		
 		return "hello";
 	}
@@ -67,10 +95,5 @@ public class GroceryInventory {
 			e.printStackTrace();
 		}
 		return "[]";
-	}
-	
-	public static void main()
-	{
-		String result = getGrocery("1");
 	}
 }

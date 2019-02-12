09a1e479718635a145c3c6236eec0b63d5773134
jimting <handsome841206@gmail.com>
2019-02-13 00:22:49 +0800

    Update GroceryInventoryController.java

--- src/main/java/GroceryInventory/GroceryInventoryController.java
+++ src/main/java/GroceryInventory/GroceryInventoryController.java
@@ -19,7 +19,7 @@ public class GroceryInventoryController {
 	
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
 	@CrossOrigin(origins = "*")
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() 
     {
 		return "success";

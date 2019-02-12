495714f955607f63c8ad110c3fbf2e5c0a7e5e61
jimting <handsome841206@gmail.com>
2019-02-13 00:11:55 +0800

    Update GroceryInventoryController.java

--- src/main/java/GroceryInventory/GroceryInventoryController.java
+++ src/main/java/GroceryInventory/GroceryInventoryController.java
@@ -12,10 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
-
-@Api(tags = "Book")
 @RestController
 public class GroceryInventoryController {
 	@Autowired

42f97f9f34f3f10a11d88a19894b602660b76ba3
jimting <handsome841206@gmail.com>
2019-02-21 14:31:28 +0800

    Update GroceryInventoryTest.java

--- src/test/java/GroceryInventory/GroceryInventoryTest.java
+++ src/test/java/GroceryInventory/GroceryInventoryTest.java
@@ -1,15 +1,57 @@
 package GroceryInventory;
 
+import static org.junit.Assert.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
-public class GroceryInventoryTest {
-
+public class GroceryInventoryTest 
+{
+	private GroceryInventoryController groceryInventory = new GroceryInventoryController();
+	
+	//controller's test
+	
 	@Test
-	public void testDatabase()
+	//index
+	public void testIndex()
 	{
-		//測試是否成功連線到DB
-		//if(GroceryInventory.getGrocery(userID))System.out.println("答對了！");
-		//else	System.out.println("正確答案應該是5");
+		assertEquals("success", groceryInventory.index());
+	}
+	
+	@Test
+	public void testGetGrocery()
+	{
+		assertTrue(isJSONValid(groceryInventory.getGrocery()));
 	}
 
+	@Test
+	public void testGetGroceryByID()
+	{
+		String ID = "5c49e70e212d8d18c0fccd55";
+		assertTrue(isJSONValid(groceryInventory.getGroceryByID(ID)));
+	}
+	
+	@Test
+	public void testGetNotification()
+	{
+		assertTrue(isJSONValid(groceryInventory.getNotification("1")));
+	}
+	
+	
+	
+	public boolean isJSONValid(String test) {
+	    try {
+	        new JSONObject(test);
+	    } catch (JSONException ex) {
+	        // edited, to include @Arthur's comment
+	        // e.g. in case JSONArray is valid as well...
+	        try {
+	            new JSONArray(test);
+	        } catch (JSONException ex1) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
 }

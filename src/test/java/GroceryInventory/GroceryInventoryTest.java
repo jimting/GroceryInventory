319dc6a1dcc207f8e784cc87e6d5bab6c6a12c52
Chen <i11007020@gmail.com>
2019-07-12 18:35:11 +0800

    add annotations @RunWith, @SpringBootTest and @Autowired

--- src/test/java/GroceryInventory/GroceryInventoryTest.java
+++ src/test/java/GroceryInventory/GroceryInventoryTest.java
@@ -1,14 +1,23 @@
 package GroceryInventory;
 
-import static org.junit.Assert.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GroceryInventoryTest 
 {
-	private GroceryInventoryController groceryInventory = new GroceryInventoryController();
+	@Autowired
+	private GroceryInventoryController groceryInventory;
 	
 	//controller's test
 	
